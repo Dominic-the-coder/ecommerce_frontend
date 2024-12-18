0efabc6eefc6392987da9e3d8ea6d0f73bb3f86b
Dominic Fam <famdominic1218@gmail.com>
2024-12-18 15:00:01 +0800

    commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Toaster } from "sonner";
 
 import Products from "./pages/Products";
+import ProductAddNew from "./pages/ProductAddNew";
+import ProductEdit from "./pages/ProductEdit";
 
 function App() {
   return (
     <div className="App">
+      {" "}
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Products />} />
+          <Route path="/products/new" element={<ProductAddNew />} />
+          <Route path="/products/:id/edit" element={<ProductEdit />} />
         </Routes>
       </BrowserRouter>
+      <Toaster richColors position="top-right" />
     </div>
   );
 }
--- src/constants.js
+++
@@ -1,2 +0,0 @@
-export const API_URL =
-  "https://orange-acorn-w9pjr7v6pj357q-5555.app.github.dev";
